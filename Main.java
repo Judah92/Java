235b632ecf2c14afc671b95c9dcf8ad1bde4ac62
Judah92 <mattscallorn@gmail.com>
2024-01-21 16:04:30 -0600

    first assignment Testing OutPut after successful code completion, compilation, and testing.

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args)
+    {
+        String myName = "John Roby";
+        String myName2 = "Richard Blaine";
+            System.out.println(myName);
+            System.out.println(myName2);
+        myName2 = myName;
+        myName = "Harper Blaine";
+            System.out.println(myName);
+            System.out.println(myName2);
+    }
+
+}
