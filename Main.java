33d8be1e2b9cd744bdae55527250783f192399e6
Judah92 <mattscallorn@gmail.com>
2024-02-04 16:03:24 -0600

    Creeated Main and instantiated MyCar objects for the default constructor, value pass constructor, and the Mutator method.

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
-public class Main {
+public class Main 
+{
     public static void main (String[] args)
     {
-        String myName = "John Roby";
-        String myName2 = "Richard Blaine";
-            System.out.println(myName);
-            System.out.println(myName2);
-        myName2 = myName;
-        myName = "Harper Blaine";
-            System.out.println(myName);
-            System.out.println(myName2);
+        MyCar firstCar = new MyCar(); //default constructor: Default Values. Ref: in class example 1_25
+        MyCar newCar_vp = new MyCar(1200, 60, 5); //Value Pass constructor: Miles, Speed, Gear. Ref: in class example 1_25
+        
+        System.out.println("Default Constructor Values: " + firstCar.get_o() + ", " 
+        + firstCar.get_cg() + ", " + firstCar.get_cs()); //Printing default constructor values
+        
+        firstCar.mutator(800, 55, 4); //Setting Mutator values for firstCar object
+        System.out.println ("Mutator Values: "  + firstCar.get_o() 
+        + ", " + firstCar.get_cg() + ", " + firstCar.get_cs());// and printing values
+        
+        System.out.println("Value Pass Constructor Values: " + newCar_vp.get_o() 
+        + ", " + newCar_vp.get_cg() + ", " + newCar_vp.get_cs()); //Printing value pass constructor values
+
     }
 
 }
